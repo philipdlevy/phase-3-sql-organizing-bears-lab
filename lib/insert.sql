c5fa59404875be1da84724f4ab2dd82f62df2f88
Philip Levy <philipdlevy@gmail.com>
2022-06-05 14:46:05 -0600

    commit tree 0f7140392a170f087d9a0f47b99b3f13bac916df

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 2, "M", "Dark Brown", "Sweet", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 12, "M", "Brown", "Kind", 0);
+INSERT INTO bears VALUES (3, "Tabitha", 2, "F", "White", "Funny", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 3, "M", "Brown", "Silly", 1);
+INSERT INTO bears VALUES (5, "Melissa", 4, "F", "Grey", "Stupid", 0);
+INSERT INTO bears VALUES (6, "Grinch", 7, "M", "Tan", "Clumsy", 1);
+INSERT INTO bears VALUES (7, "Wendy", 8, "F", "Dark Tan", "Caring", 1);
+INSERT INTO bears VALUES (8, Null, 10, "F", "Black", "Mean", 0);
